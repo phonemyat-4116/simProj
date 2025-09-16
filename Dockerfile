da8698440993b7af7b5883c5a70386e51bad8916
Phone Myat Thu <myat57116@gmail.com>
2025-09-16 16:45:52 +0630

    A few update

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,4 @@ FROM openjdk:21
 COPY ./target/classes/com /tmp/com
 WORKDIR /tmp
 ENTRYPOINT ["java", "com.napier.sem.App"]
+
--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -6,3 +6,13 @@ public class App {
         System.out.println("Boo yah!");
     }
 }
+
+/*
+✅ Key points:
+
+Runner’s JDK 20 is only used for compiling outside Docker.
+
+Docker JDK 21 is only used for running the code inside the container.
+
+No error occurs because compiled Java 20 classes are compatible with JDK 21.
+ */
