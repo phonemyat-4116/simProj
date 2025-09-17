c9c159406618d60d848304bfcc840ece6642d196
Phone Myat Thu <myat57116@gmail.com>
2025-09-17 17:41:36 +0630

    Add mongoDB dependency and establish Connection

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,9 +1,31 @@
 package com.napier.sem;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 public class App {
     public static void main(String[] args)
     {
-        System.out.println("Boo yah!");
+        MongoDatabase database;
+        try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
+            database = mongoClient.getDatabase("db");
+
+            MongoCollection<Document> collection = database.getCollection("test");
+
+            Document doc = new Document("name", "Phone Myat")
+                    .append("class", "Software Engineering")
+                    .append("year", "2025")
+                    .append("result", new Document("CW", 95).append("EX", 85));
+            collection.insertOne(doc);
+
+            Document myDoc = collection.find().first();
+            System.out.println(myDoc.toJson());
+
+        }
+
     }
 }
 
