6a52e22b5b42fcacf71c8c228f1eea41131c97c9
Phone Myat Thu <myat57116@gmail.com>
2025-09-23 23:45:16 +0630

    Testing for employees' details

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -111,9 +111,17 @@ public class App {
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT emp_no, first_name, last_name "
-                            + "FROM employees "
-                            + "WHERE emp_no = " + ID;
+                    "SELECT e.emp_no, e.first_name, e.last_name, " +
+                            "t.title, s.salary, d.dept_name, " +
+                            "CONCAT(mgr.first_name, ' ', mgr.last_name) as manager " +
+                            "FROM employees e " +
+                            "LEFT JOIN titles t ON e.emp_no = t.emp_no AND t.to_date = '9999-01-01' " +
+                            "LEFT JOIN salaries s ON e.emp_no = s.emp_no AND s.to_date = '9999-01-01' " +
+                            "LEFT JOIN dept_emp de ON e.emp_no = de.emp_no AND de.to_date = '9999-01-01' " +
+                            "LEFT JOIN departments d ON de.dept_no = d.dept_no " +
+                            "LEFT JOIN dept_manager dm ON de.dept_no = dm.dept_no AND dm.to_date = '9999-01-01' " +
+                            "LEFT JOIN employees mgr ON dm.emp_no = mgr.emp_no " +
+                            "WHERE e.emp_no = " + ID;
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Return new employee if valid.
@@ -124,6 +132,10 @@ public class App {
                 emp.emp_no = rset.getInt("emp_no");
                 emp.first_name = rset.getString("first_name");
                 emp.last_name = rset.getString("last_name");
+                emp.title = rset.getString("title");
+                emp.salary = rset.getInt("salary");
+                emp.dept_name = rset.getString("dept_name");
+                emp.manager = rset.getString("manager");
                 return emp;
             }
             else
@@ -137,6 +149,24 @@ public class App {
         }
     }
 
+    public void displayEmployee(Employee emp){
+        if(emp != null)
+        {
+            System.out.println(
+                    emp.emp_no + " "
+                            + emp.first_name + " "
+                            + emp.last_name + "\n"
+                            + emp.title + "\n"
+                            + "Salary:" + emp.salary + "\n"
+                            + emp.dept_name + "\n"
+                            + "Manager: " + emp.manager + "\n");
+        }
+        else
+        {
+            System.out.println("Employee not found!");
+        }
+    }
+
     public static void main(String[] args)
     {
         // Create new Application
@@ -145,8 +175,23 @@ public class App {
         // Connect to database
         a.connect();
 
+        // Get Employee
+        Employee emp = a.getEmployee(255530);
+
+        // Check if employee was found
+        if (emp == null) {
+            System.out.println("Employee " + emp.emp_no + " is not found in database!");
+        } else {
+            System.out.println("Employee found: " + emp.emp_no);
+        }
+
+        // Display results
+        a.displayEmployee(emp);
+
         // Disconnect from database
         a.disconnect();
+
+        System.out.println("Application finished.");
     }
 }
 
