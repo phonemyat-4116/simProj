eaf7954143406aa30f61f0b3dd8d9238cdf6c90a
Phone Myat Thu <myat57116@gmail.com>
2025-10-24 18:18:37 +0630

    Modify App, Docker Compose, App Docker and pom.xml to make app and db run separately

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -47,7 +47,7 @@ public class App {
     /**
      * Connect to the MySQL database.
      */
-    public void connect() {
+    public void connect(String location, int delay) {
         try {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -57,18 +57,27 @@ public class App {
         }
 
         int retries = 10;
+        boolean shouldWait = false;
         for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
             try {
-                // Wait a bit for db to start
-                Thread.sleep(30000);
+                if(shouldWait){
+                    // Wait a bit for db to start
+                    Thread.sleep(delay);
+                }
+
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://" + location
+                                + "/employees?allowPublicKeyRetrieval=true&useSSL=false",
+                        "root", "example");
                 System.out.println("Successfully connected");
                 break;
             } catch (SQLException sqle) {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println("Failed to connect to database attempt " + i);
                 System.out.println(sqle.getMessage());
+
+                // Let's wait before attempting to reconnect
+                shouldWait = true;
             } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
@@ -351,8 +360,14 @@ public class App {
         // Create new Application
         App a = new App();
 
-        // Connect to database
-        a.connect();
+        // Connect to Database
+        if(args.length < 1){
+            // port map in docker compose
+            a.connect("localhost:33060", 10000);
+        }
+        else{
+            a.connect(args[0], Integer.parseInt(args[1]));
+        }
 
         // Get Employee
 //        Employee emp = a.getEmployee(255530);
@@ -396,4 +411,10 @@ Runner’s JDK 20 is only used for compiling outside Docker.
 Docker JDK 21 is only used for running the code inside the container.
 
 No error occurs because compiled Java 20 classes are compatible with JDK 21.
+
+Because your main() method supports two modes automatically:
+
+Scenario	Command	Database host	Port used	Works with
+🧑‍💻 Local run	java -jar seMethods.jar	localhost	33060	Local / exposed DB port
+🐳 Docker Compose	java -jar seMethods.jar db:3306 10000	db	3306	Internal Docker network
  */
