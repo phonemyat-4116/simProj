3f8cf69c4a9a7fbc4bc35f33617743f860566c53
Phone Myat Thu <myat57116@gmail.com>
2025-10-30 09:57:52 +0630

    Initialize SpingApplication

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class GreetingController {
+    private long counter = 0;
+    private static final String template = "Hello, %s!";
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name)
+    {
+        return new Greeting(counter++, String.format(template, name));
+    }
+}
